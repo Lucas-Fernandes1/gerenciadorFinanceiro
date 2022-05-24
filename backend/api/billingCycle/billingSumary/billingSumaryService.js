00bf27edf7d8f8ddaa5cf11f5018bf595b78dd54
Lucas Fernandes <fernandeslucas960@gmail.com>
2022-05-24 00:19:50 -0300

    Refatoração do método "getSummary".

--- backend/api/billingCycle/billingSumary/billingSumaryService.js
+++ backend/api/billingCycle/billingSumary/billingSumaryService.js
@@ -2,23 +2,17 @@ const _ = require('lodash');
 const BillingCycle = require('../billingCycle');
 
 function getSummary(req, res) {
-    BillingCycle.aggregate({
-        $project: { credit: { $sum: "$credits.value" }, debts: { $sum: "$debts.value" } }
+    BillingCycle.aggregate([{
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
     }, {
-        $group: {
-            _id: null,
-            credit: {
-                $sum: "$credit"
-            }, debt: {
-                $sum: "$debt"
-            }
-        },
-        $project: { _id: 0, credit: 1, debt: 1 }, function(error, result) {
-            if(error) {
-                res.status(500).json({errors: [error]});
-            } else {
-                res.json(_.defaults(result[0], {credit : 0, debit: 0 }));
-            }
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+    }, {
+        $project: { _id: 0, credit: 1, debt: 1 }
+    }], function (error, result) {
+        if (error) {
+            res.status(500).json({ errors: [error] });
+        } else {
+            res.json(_.defaults(result[0], { credit: 0, debt: 0 }))
         }
     })
 }
