c3b75a2550c1ce8cb411f924381db18f9d78ccda
Lucas Fernandes <fernandeslucas960@gmail.com>
2022-05-24 00:16:57 -0300

    Criação do método "getSumary".

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,8 +1,12 @@
 const BillingCycle = require('./billingCycle');
 
+//CRUD
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
+
+//Validações
 BillingCycle.updateOptions({new: true, runValidators: true});
 
+//Rota Count
 BillingCycle.route('count', function(req, res, next) {
     BillingCycle.count(function(error, value) {
         if(error) {
