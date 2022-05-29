1ba0de93f69dc06a24183609b0f8ad8b6d3f2508
Lucas Fernandes <fernandeslucas960@gmail.com>
2022-05-29 15:56:14 -0300

    Implementando html do header, inside e footer.

--- frontend/gulpTasks/server.js
+++ frontend/gulpTasks/server.js
@@ -1,19 +1,18 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
 const watch = require('gulp-watch')
 const webserver = require('gulp-webserver')
-//Gulp watch ficará observando os arquivos indicados.
 
 gulp.task('watch', () => {
-    watch('app/**/*.html', () => gulp.start('app.html'))
-    watch('app/**/*.css', () => gulp.start('app.css'))
-    watch('app/**/*.js', () => gulp.start('js.app'))
-    watch('app/**/*.*', () => gulp.start('app.assets'))
-});
+  watch('app/**/*.html', () => gulp.start('app.html'))
+  watch('app/**/*.css', () => gulp.start('app.css'))
+  watch('app/**/*js',  () => gulp.start('app.js'))
+  watch('assets/**/*.*', () => gulp.start('app.assets'))
+})
 
 gulp.task('server', ['watch'], () => {
-    return gulp.src('public').pipe(webserver({
-        livereload: true,
-        port: 3000,
-        open: true      
-    }))
+  return gulp.src('public').pipe(webserver({
+    livereload: true,
+    port: 3000,
+    open: true
+  }))
 })
