0fbdfb0ea94e477f1e46deddab67ae4d82dde731
Lucas Fernandes <fernandeslucas960@gmail.com>
2022-05-29 14:45:27 -0300

    Finalização da configuração da build.

--- frontend/gulpTasks/server.js
+++ frontend/gulpTasks/server.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp');
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
 //Gulp watch ficará observando os arquivos indicados.
 
 gulp.task('watch', () => {
-
+    watch('app/**/*.html', () => gulp.start('app.html'))
+    watch('app/**/*.css', () => gulp.start('app.css'))
+    watch('app/**/*.js', () => gulp.start('js.app'))
+    watch('app/**/*.*', () => gulp.start('app.assets'))
 });
 
 gulp.task('server', ['watch'], () => {
-
+    return gulp.src('public').pipe(webserver({
+        livereload: true,
+        port: 3000,
+        open: true      
+    }))
 })
